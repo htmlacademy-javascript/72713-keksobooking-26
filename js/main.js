4820a9c15aa411bd50ffad29cbac0c4a0dcb60a6
Евгения <jenny91@bk.ru>
2022-09-17 23:08:00 +0300

    zadanie-9.9

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import {createManyAds} from './data.js';
-import {renderNewOffer} from './offers.js';
-import {addDisabled} from './form.js';
+//import {renderNewOffer} from './offers.js';
+//import {addDisabled} from './form.js';
+import {initMap} from './map.js';
 
-const ADS_NUMBERS = 1;
-renderNewOffer(createManyAds(ADS_NUMBERS));
+const ADS_NUMBERS = 10;
+//renderNewOffer(createManyAds(ADS_NUMBERS));
 
-addDisabled(true);
+//addDisabled(true);
+const boo = createManyAds(ADS_NUMBERS);
+initMap(boo);
